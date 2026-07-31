2f010788bc86aac3c849ad4150f039aea64ff4be
Duncan <mofiamadiback@gmail.com>
2026-07-31 11:07:36 +0300

    Register seed blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from .config import Config
 from .extensions import db, jwt, bcrypt, cors
 from flask_migrate import Migrate
 from .models import User, Department, Course, Enrollment
-from .routes.seed import seed_bp
 
 def create_app():
     app = Flask(__name__)
@@ -19,15 +18,16 @@ def create_app():
     bcrypt.init_app(app)
     cors.init_app(app)
 
-    from .routes import (auth_bp, department_bp, course_bp, enrollment_bp, user_bp,)
+    from .routes import (auth_bp, department_bp, course_bp, enrollment_bp, user_bp, )
+    from .routes.seed import seed_bp
 
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
     app.register_blueprint(department_bp, url_prefix="/api/departments")
     app.register_blueprint(course_bp, url_prefix="/api/courses")
     app.register_blueprint(enrollment_bp, url_prefix="/api/enrollments")
     app.register_blueprint(user_bp, url_prefix="/api/users")
-    app.register_blueprint(user_bp, url_prefix="/api")
-
+    app.register_blueprint(seed_bp, url_prefix="/api")
+    
     @app.route("/")
     def home():
         return {
